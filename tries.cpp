d8f3efc10a741b6b1ccb3cd7b24408a125c78674
Anthony Alberto <2023260043@ub.edu.bz>
2025-11-14 20:44:45 +0000

    added insert function

--- tries.cpp
+++ tries.cpp
@@ -3,9 +3,38 @@
 #include <string>
 using namespace std;
 
+struct TrieNode {
+    bool isEndOfWord;
+    TrieNode* children[26];
+};
 
+
+TrieNode* createNode() {
+    TrieNode* node = new TrieNode();
+    node->isEndOfWord = false;
+    for (int i = 0; i < 26; i++)
+        node->children[i] = nullptr;
+    return node;
+}
+
+void insert(TrieNode* root, const string& word);
 
 int main() {
 
 	return 0;
+}
+
+void insert(TrieNode* root, const string& word) {
+    TrieNode* curr = root;
+
+    for (char ch : word) {
+        if (!isalpha(ch)) continue;
+        int idx = tolower(ch) - 'a';
+
+        if (curr->children[idx] == nullptr)
+            curr->children[idx] = createNode();
+
+        curr = curr->children[idx];
+    }
+    curr->isEndOfWord = true;
 }
