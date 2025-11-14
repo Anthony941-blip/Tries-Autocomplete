45e10abeceff4a0f3ee8567c092dc8f42c965904
Anthony Alberto <2023260043@ub.edu.bz>
2025-11-14 20:48:03 +0000

    added search prefix function

--- tries.cpp
+++ tries.cpp
@@ -16,7 +16,28 @@ TrieNode* createNode() {
     return node;
 }
 
+
+
+TrieNode* searchPrefix(TrieNode* root, const string& prefix);
+
 int main() {
 
 	return 0;
+}
+
+
+
+TrieNode* searchPrefix(TrieNode* root, const string& prefix) {
+    TrieNode* curr = root;
+
+    for (char ch : prefix) {
+        if (!isalpha(ch)) continue;
+        int idx = tolower(ch) - 'a';
+
+        if (!curr->children[idx])
+            return nullptr;
+
+        curr = curr->children[idx];
+    }
+    return curr;
 }
